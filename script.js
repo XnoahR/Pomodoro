30b35288b1edc398369a97fccb1a5050c0baf106
Rio Saputro <rio.ae23@gmail.com>
2023-11-13 20:49:09 +0700

    fix again

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ var pomodoro = 1;
 var shortBreak = 5;
 var longBreak = 15;
 
-var totalSession = 4;
+var totalSession = 0;
 
 isWork = true;
 const pomodoroSession = document.getElementById("pomodoro-session");
 const shortBreakSession = document.getElementById("short-break-session");
 const longBreakSession = document.getElementById("long-break-session");
 const startButton = document.getElementById("start");
-
+let mode;
 let seconds = "00";
 
 window.onload = () => {
-    document.getElementById("minute").innerHTML = pomodoro;
+    mode=pomodoro;
+    document.getElementById("minute").innerHTML = mode;
     document.getElementById("second").innerHTML = seconds;
 
     isWork = true;
@@ -26,23 +27,37 @@ window.onload = () => {
 
     startButton.addEventListener("click", () => {
         let seconds = "3";
-        let pomodoroTime = pomodoro - 1;
+        let minute = mode - 1;
     
         timeFunction = setInterval(function () {
-            document.getElementById("minute").innerHTML = pomodoroTime;
+            document.getElementById("minute").innerHTML = minute;
             document.getElementById("second").innerHTML = seconds;
     
             seconds--;
             if (seconds < 10) {
                 seconds = "0" + seconds;
             }
-            if(pomodoroTime == 0 && seconds == 0){
+            if(minute === 0 && seconds === "0-1"){
+                seconds = "00";
+                isWork = false;
+                totalSession++;
+                if(totalSession%4 == 0){
+                    mode = longBreak;
+                    document.getElementById("minute").innerHTML = longBreak;
+                    seconds = "00";
+                }
+                else{
+                    mode = shortBreak;
+                    document.getElementById("minute").innerHTML = shortBreak;
+                    seconds = "00";
+                }
                 clearInterval(timeFunction);
+
             }
-            if (seconds == 0) {
-                pomodoroTime--;
-                if (pomodoroTime < 10 && pomodoroTime >= 0) {
-                    pomodoroTime = "0" + pomodoroTime;
+            if (seconds === "0-1") {
+                minute--;
+                if (minute < 10 && minute >= 0) {
+                    minute = "0" + minute;
                 }
                 seconds = "59";
             }
